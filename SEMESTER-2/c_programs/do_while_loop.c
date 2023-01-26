aef194687a6988c4235788b63e07290495064ccf
kusal6199 <kushalkdl63@gmail.com>
2023-01-26 15:52:39 +0545

    add: while loop program

---
+++ SEMESTER-2/c_programs/do_while_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("enter the number: ");
+    scanf("%d", & num);
+    int sum; 
+
+    int num = 0;
+    do {
+         printf("%d\n", num);
+        i=i+1;
+    }
+    while(i<=10);
+       
+
+
+}
