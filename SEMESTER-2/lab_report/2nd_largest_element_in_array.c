817d45a3342108111ab711136349b2570bda4ae4
kusal6199 <kushalkdl63@gmail.com>
2023-02-12 17:46:25 +0545

    updated code of second max element of array

--- SEMESTER-2/lab_report/2nd_largest_element_in_array.c
+++ SEMESTER-2/lab_report/2nd_largest_element_in_array.c
@@ -1,34 +1,26 @@
 #include<stdio.h>
 void main()
 {
-    int n,i,max2,max1;
+    int n,i,max2=0,max1=0;
     printf("enter the array size: ");
     scanf("%d",&n);
     int a[n];
     printf("enter the array elements:\n ");
     
-    for ( i = 0; i < n; i++)
+    while(n>0)
     {
         scanf("%d",&a[i]);
-    }
-    max1=a[0];
-    for ( i = 1; i < n; i++)
-    {
-        if (a[i]>=max1)
-        max1=a[i];   
-    }
-    for ( i = 1; i < n; i++)
-    {
-        while (a[i]<max1)
+        if(a[i]>max1)
         {
-           max2=a[i];
-           if (max2>a[i])
-           {
-            max2=a[i];
-           }
-            i++;
+        	max2=max1;
+            max1=a[i];
         }
-        
+        if(a[i]>max2 && a[i]<max1)
+        {
+            max2=a[i];
+        }
+        n--;
     }
+   
     printf("the second max num. is %d",max2);
 }
