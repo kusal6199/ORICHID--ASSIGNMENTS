943515f29579c24bd0f530e95a09defe0baac3e3
kusal6199 <kushalkdl63@gmail.com>
2023-02-12 20:49:48 +0545

    added C program that count odd even +ve -ve element inside array

--- SEMESTER-2/lab_report/count_display_positive_neg_even_odd_inArray.c
+++ SEMESTER-2/lab_report/count_display_positive_neg_even_odd_inArray.c
@@ -3,5 +3,37 @@
 #include<stdio.h>
 void main()
 {
+    int n,i,po=0,ng=0,odd=0,evn=0;
+    printf("enter the array size: ");
+    scanf("%d",&n);
+    int a[n];
+
+    printf("enter any numbers i.e positive, negative, odd, and even numbers :  ");
+    for ( i = 0; i < n; i++)
+    {
+       scanf("%d",&a[i]);
+    }
+    for ( i = 0; i < n; i++)
+    {
+        if (a[i]==0)
+        continue;
+        else if(a[i]>=0)
+        po++;
+        else
+        ng++;    
+    }
+    for ( i = 0; i < n; i++)
+    {
+        if (a[i]%2==0)
+        evn++;
+        else
+        odd++;
+     
+        
+    }
+    printf("no.of +ve element = %d\nno.of -ve element = %d\nno.of even element = %d\nno.of odd element = %d\n",po,ng,evn,odd);
     
+    
+    
+
 }
