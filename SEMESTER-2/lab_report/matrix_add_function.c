2e1a3199306225fbb2a491b71dd2b492cd7f62fb
kusal6199 <kushalkdl63@gmail.com>
2023-02-18 10:16:47 +0545

    added C file that add 3*3 matrix using function

--- SEMESTER-2/lab_report/matrix_add_function.c
+++ SEMESTER-2/lab_report/matrix_add_function.c
@@ -1,14 +1,49 @@
 #include<stdio.h>
-int matrixadd(int (mat1[2][2]), int (mat2[2][2]));
+void matrixadd(int mat1[3][3],int mat2[3][3]);
 
 int main(){
-    int mat1[2][2]={{1,2},{3,4}};
-    int mat2[2][2]={{7,5},{6,8}};
-
+    int mat1[3][3],mat2[3][3],i,j;
+     printf("\nEnter elements of 1st matrix:\n");
+    for (i = 0; i < 3; ++i)
+    for (j = 0; j < 3; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &mat1[i][j]);
+    }
+  
+  printf("Enter elements of 2nd matrix:\n");
+    for (i = 0; i < 3; ++i)
+    for (j = 0; j < 3; ++j) {
+      printf("Enter element b%d%d: ", i + 1, j + 1);
+      scanf("%d", &mat2[i][j]);
+    }
     
-   return 0;
+    matrixadd(mat1,mat2);
+
+
+
+    return 0;
 }
 
-int matrixadd(int mat1[2][2],int mat2[2][2]){
+void matrixadd(int mat1[3][3],int mat2[3][3]){
+    int i,j;
+    int sum[3][3];
+    for (i = 0; i < 3; i++)
+    {
+       for(j=0;j<3;j++)
+       {
+        sum[i][j]=mat1[i][j]+mat2[i][j];
+       }
+    }
+    printf("\nSum of two matrices: \n");
+    for (i = 0; i < 3; ++i)
+    {
+       for (j = 0; j < 3; ++j) {
+       printf("%d   ", sum[i][j]);
+       if (j == 3-1) {
+        printf("\n\n");
+      }
+    }
+    }
+
     
 }
