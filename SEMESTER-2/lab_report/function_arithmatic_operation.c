11c013d7b57c780f43cfac5f569d3656cb9a2cad
kusal6199 <kushalkdl63@gmail.com>
2023-02-19 08:55:10 +0545

    Program in C to perform basic arithmetic operations using function

---
+++ SEMESTER-2/lab_report/function_arithmatic_operation.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &num1, &num2);
+    
+    
+    printf("Sum = %d\n", add(num1, num2));
+    printf("Difference = %d\n", subtract(num1, num2));
+    printf("Product = %d\n", multiply(num1, num2));
+    printf("Quotient = %.2f\n", divide(num1, num2));
+    return 0;
+}
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+
+float divide(int a, int b) {
+    if (b == 0) {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+    return (float)a / b;
+}
