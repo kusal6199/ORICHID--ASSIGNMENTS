488405e33f76858a469d12b6d97129fc79819f2e
kusal6199 <kushalkdl63@gmail.com>
2023-02-20 20:30:23 +0545

    added C file to convert lowercase into uppercase and viceversa using pointer

---
+++ SEMESTER-2/lab_report/case_conversion_using_pointer.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    int i;
+    printf("enter alphabitical chracter: ");
+    scanf("%c",&a);
+
+    char *ptr=&a;
+    
+   while (*ptr)
+   {
+     if (*ptr>='a' && *ptr<='z'){
+        *ptr=*ptr-32;
+        printf("after conversion:%c",*ptr);
+        }
+     else if (*ptr>='A' && *ptr<='Z'){
+        *ptr=*ptr-32;
+        printf("after conversion:%c",*ptr);
+     }
+    // else
+    // printf("\nnot valid chractr");
+
+    ptr++;
+     
+   }
+   
+   
+    
+}
