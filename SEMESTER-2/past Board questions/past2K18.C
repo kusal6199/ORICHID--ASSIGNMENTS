cf385f222e3cb438219799c5144859c289628b8e
kusal6199 <kushalkdl63@gmail.com>
2023-03-14 23:15:45 +0545

    c file handeling

---
+++ SEMESTER-2/past Board questions/past2K18.C
@@ -0,0 +1,12 @@
+// file handeling
+#include<stdio.h>
+int main(){
+    FILE *fp;
+    fp=fopen("BCA.txt","w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    fprintf(fp,"welcome to BCA");
+    printf("File created and written successfully.\n");
+}
