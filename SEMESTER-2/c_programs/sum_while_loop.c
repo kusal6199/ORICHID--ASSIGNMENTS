17d7a4cf208d7b92c96184914aea0c3d9024a70f
kusal6199 <kushalkdl63@gmail.com>
2023-03-15 15:31:17 +0545

    Program to sum all integers from 1 to 100 using while loop.

---
+++ SEMESTER-2/c_programs/sum_while_loop.c
@@ -0,0 +1,12 @@
+// Program to sum all integers from 1 to 100 using while loop.
+#include<stdio.h>
+int main()
+{
+    int i=0,sum=0;
+    while (i<=5)
+    {
+       sum=sum+i;
+       i++;
+    }
+    printf("the sum is %d",sum);
+}
