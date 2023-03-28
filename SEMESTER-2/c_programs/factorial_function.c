837941496c0c1f93f93afd35eaf88cd6d652fe8f
kusal6199 <kushalkdl63@gmail.com>
2023-03-28 22:47:41 +0545

    added c file

--- SEMESTER-2/c_programs/factorial_function.c
+++ SEMESTER-2/c_programs/factorial_function.c
@@ -18,3 +18,29 @@ void fact(int a)
     }
     printf("the factorial of number is %d",fact);
 }
+
+// #include<stdio.h>
+// int factorial(int x);
+
+// int factorial(int a){
+//     int fact=1;
+//     int i,n;
+//     printf("enter a number: ");
+//     scanf("%d",&n);
+
+//     for ( i = n; i>0; i--)
+//     {
+//         fact=fact*i;
+//     }
+//     return(fact);
+    
+// }
+// void main(){
+//      int n;
+//     // printf("enter a number: ");
+//     // scanf("%d",&n);
+//     int ans=factorial(n);
+//     printf("the factorial is %d",ans);
+
+
+// }
