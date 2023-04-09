bf2481a2734c11b89b997b7b7e058dd0dc9ccced
kusal6199 <kushalkdl63@gmail.com>
2023-04-09 16:09:12 +0545

    DMA file to find min and max number

---
+++ SEMESTER-2/c_programs/DMA.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr;
+    int i,min,max,n=4;
+    ptr=(int*)malloc(n*sizeof(int));
+    if (ptr==NULL)
+    {
+        printf("fail");
+    }
+    
+    printf("enter the numbers\n");
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    max=min=ptr[0];
+    for ( i = 0; i < n; i++)
+    {
+        if (max<=ptr[i])
+        {
+            max=ptr[i];
+        }
+        if (min>=ptr[i])
+        {
+            min=ptr[i];
+        }
+        
+    }
+    printf("the max is %d\nmin is %d",max,min);
+    
+    return 0;
+
+}
