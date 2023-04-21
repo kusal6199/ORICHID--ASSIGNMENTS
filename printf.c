95c31f01559575910dbb9add2c219e3d5d3dce16
kusal6199 <kushalkdl63@gmail.com>
2023-04-21 21:43:43 +0545

    edited

--- printf.c
+++ printf.c
@@ -16,32 +16,7 @@ int main(){
     printf("Ritesh jung lama"); printf("kushal");
     printf("mausam sunar");
     printf("sunar mausam");
-    printf("sunar mausam");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("Ritesh jung lama");
-   printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("Ritesh jung lama");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("Ritesh jung lama");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-     printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
-    printf("kushal kandel");
+   
     printf("kushal kandel");
     printf("kushal kandel");
    
